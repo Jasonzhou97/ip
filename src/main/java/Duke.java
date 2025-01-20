2ed31ddefae00da18ae4f6ad445be0890d5e998d
Jasonzhou97 <cheetahjason168@gmail.com>
2025-01-20 14:18:47 +0800

    added level-1 functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,6 +11,13 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Lebum");
         System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while(!(input=sc.nextLine()).equalsIgnoreCase("bye")){
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
+
+
     }
 }
